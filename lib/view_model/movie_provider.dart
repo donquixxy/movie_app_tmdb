3f7558f9c89d76e26d54095d90029d30d9198ba9
donquixxy <agusary726@gmail.com>
2022-07-31 12:18:58 +0800

    provider refactor

--- lib/view_model/movie_provider.dart
+++ lib/view_model/movie_provider.dart
@@ -4,17 +4,10 @@ import 'package:movie_app_tmdb/models/movie_models.dart';
 
 class MovieProvider extends ChangeNotifier {
   final ApiProvider _provider = ApiProvider();
-  // final List<MovieModels> _movieList = [];
   MovieModels? movieModels;
-
-  // UnmodifiableListView<MovieModels> get dataList =>
-  //     UnmodifiableListView<MovieModels>(_movieList);
-
-  // List<MovieModels> get listMovie => _movieList;
 
   Future<void> fetchPopularMovie() async {
     var result = await _provider.getPopularMovie();
-    // _movieList.add(result!);
     movieModels = result!;
     notifyListeners();
   }
